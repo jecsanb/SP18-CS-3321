27f3c33469a94943bcb59452c38c39583b4f3ab4
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-02-17 13:44:38 -0600

    Lab started

---
+++ lab02/DjiaAverage.java
@@ -0,0 +1,21 @@
+/*
+ * DjiaAverage.java
+ * Copyright (C) 2018 Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
+ *
+ *   @author Jecsan Blanco
+ *   @version 1.0
+ *   @since  02/17/2018
+ *
+ * This program computes the Down Jones Average from
+ * the stocks that contribute as decided by the editorial board of
+ * the Wall Street Journal.
+ */
+
+public class DjiaAverage
+{
+    DjiaAverage(){
+    } 
+    public static void main(String[] args){
+        
+	}
+}
